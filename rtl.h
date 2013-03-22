d6d4b7f1e626ff233cb5f22d1d66bf024ab01b48
Enno Rehling <enno.rehling@gmail.com>
2013-03-22 03:45:02 +0000

    configure platform-dependent funtions with cmake

--- rtl.h
+++ rtl.h
@@ -1,11 +1,16 @@
 #ifndef RTL_H
 #define RTL_H
 
-#if defined(__GNUC__)
+#include "config.h"
+#if defined(HAVE_STRINGS_H)
 #include <strings.h>
 #define strcmpl(a, b) strcasecmp(a, b)
-#elif defined(_MSC_VER)
+#elif defined(HAVE__STRICMP)
 #define strcmpl(a, b) _stricmp(a, b)
+#elif defined(HAVE_STRICMP)
+#define strcmpl(a, b) stricmp(a, b)
+#else
+#error "could not define strcmpl"
 #endif
 
 #endif
