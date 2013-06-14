1d5e25b16f29f8c5d97f9c0686925765af2997ff
Enno Rehling <enno.rehling@gmail.com>
2013-06-13 20:16:12 -0700

    time to build myself a mapper

---
+++ hexmap.c
@@ -0,0 +1,81 @@
+#include <filestream.h>
+#include <cJSON.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+static int xof, yof;
+
+static int json_getint(cJSON *json, const char *key, int def) {
+  json = cJSON_GetObjectItem(json, key);
+  return json ? json->valueint : def;
+}
+
+static const char * json_getstr(cJSON *json, const char *key, const char *def) {
+  json = cJSON_GetObjectItem(json, key);
+  return json ? json->valuestring : def;
+}
+
+void print_map(cJSON *json, FILE *F) {
+    int w = 1, h = 1;
+    int fno = 0;
+    cJSON *region, *regions = cJSON_GetObjectItem(json, "regions");
+    cJSON *faction, *factions = cJSON_GetObjectItem(json, "factions");
+    char * mapdata;
+
+    faction = cJSON_GetObjectItem(json, "faction");
+    if (faction) {
+        fno = cJSON_GetObjectItem(faction, "id")->valueint;
+    }
+    for (region=regions->child; region; region=region->next) {
+        int x = json_getint(region, "x", 0);
+        int y = json_getint(region, "y", 0);
+        if (x>=w) w=x+1;
+        if (y>=h) h=y+1;
+    }
+    mapdata = (char *)malloc(w*h*sizeof(char));
+    memset(mapdata, ' ', w*h*sizeof(char));
+    for (region=regions->child; region; region=region->next) {
+        int x = (json_getint(region, "x", 0) + xof) % w;
+        int y = (json_getint(region, "y", 0) + yof) % h;
+        const char * terrain = json_getstr(region, "terrain", 0);
+        mapdata[x+y*h] = terrain[0];
+    }
+}
+
+int main (int argc, char **argv) {
+    FILE *in = stdin, *out = stdout;
+    long len;
+    char *data;
+    cJSON *json;
+    if (argc>=1) {
+        in = fopen(argv[1], "r");
+        if (!in || errno) {
+            perror("could not open input file");
+            return errno;
+        }
+    }
+    if (argc>=2) {
+        out = fopen(argv[2], "w");
+        if (!out || errno) {
+            perror("could not open output file");
+            return errno;
+        }
+    }
+    fseek(in,0,SEEK_END);
+    len=ftell(in);
+    fseek(in,0,SEEK_SET);
+    data = (char *)malloc(len+1);
+    if (data) {
+        fread(data,1,len,in);
+    }
+    if (in!=stdin) fclose(in);
+
+    json = cJSON_Parse(data);
+    print_map(json, out);
+    free(data);
+    cJSON_Delete(json);
+    if (out!=stdout) fclose(out);
+
+    return 0;
+}
