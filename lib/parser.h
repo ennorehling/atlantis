b683eb876f4de0bf7228e1edaede2ea4fdad33ab
Enno Rehling <enno.rehling@gmail.com>
2013-08-11 21:27:49 +0200

    return types => keyword_t

--- lib/parser.h
+++ lib/parser.h
@@ -1,19 +1,11 @@
 #ifndef ATL_PARSER_H
 #define ATL_PARSER_H
 
+#include "keywords.h"
+
 struct region;
 struct faction;
 struct unit;
-
-int findkeyword(const char *s);
-int igetkeyword(const char *s);
-int getkeyword(void);
-int getitem(void);
-int getskill(void);
-int getspell(void);
-char *igetstr(const char *s);
-char *getstr(void);
-int findspell(const char *s);
 
 enum {
     U_NOTFOUND,
@@ -30,5 +22,15 @@ extern const char *terrainnames[];
 extern const char *skillnames[];
 extern const char *itemnames[][2];
 extern const char *spellnames[];
+
+keyword_t findkeyword(const char *s);
+keyword_t igetkeyword(const char *s);
+keyword_t getkeyword(void);
+int getitem(void);
+int getskill(void);
+int getspell(void);
+char *igetstr(const char *s);
+char *getstr(void);
+int findspell(const char *s);
 
 #endif
